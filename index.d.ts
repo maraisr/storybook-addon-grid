d4474fa953155dcd0d5f247d908c450beff375ae
Anton Korzunov <thekashey@gmail.com>
2021-03-21 18:28:15 +1100

    Add jsdoc

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,22 @@
+/**
+ * defines parameters for a grid system
+ */
 export interface GridParameters {
+	/**
+	 * number of columns, default 12
+	 */
 	columns?: number;
+	/**
+	 * gap between columns
+	 */
 	gap?: string;
+	/**
+	 * system's gutter (margin)
+	 */
 	gutter?: string;
+	/**
+	 * maximum allowed width
+	 */
 	maxWidth?: string;
 }
 
