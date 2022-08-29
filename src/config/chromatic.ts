83f7db3bc736820b7518f68a2a324985523d5713
Marais Rossouw <me@marais.co>
2022-08-29 18:19:59 +1000

    chore: chromatic should define its own defaults

--- src/config/chromatic.ts
+++ src/config/chromatic.ts
@@ -1,11 +1,9 @@
 import isChromatic from 'chromatic/isChromatic';
-import { parameters as defaultParemeters } from '.';
 import { withGrid } from '../components/Grids';
 import { PARAM_KEY } from '../constants';
 
 export const parameters = {
 	[PARAM_KEY]: {
-		...defaultParemeters[PARAM_KEY],
 		animation: false,
 	},
 };
