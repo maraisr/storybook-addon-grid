9566dd4502613a9903f61ffd7960e6c3a635069e
Marais Rossouw <me@marais.co>
2023-10-23 11:53:01 +1000

    chore: pika

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -88,16 +88,17 @@ export function Tools() {
 			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
 					width="24"
 					height="24"
-					viewBox="0 0 24 24"
 					fill="none"
 					stroke="currentColor"
-					strokeWidth="2"
-					strokeLinecap="round"
-					strokeLinejoin="round"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
 				>
-					<path d="M12 3h7a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-7m0-18H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h7m0-18v18"></path>
+					<path d="M3 6.2c0-1.12 0-1.68.218-2.108a2 2 0 0 1 .874-.874C4.52 3 5.08 3 6.2 3h.6c1.12 0 1.68 0 2.108.218a2 2 0 0 1 .874.874C10 4.52 10 5.08 10 6.2v11.6c0 1.12 0 1.68-.218 2.108a2 2 0 0 1-.874.874C8.48 21 7.92 21 6.8 21h-.6c-1.12 0-1.68 0-2.108-.218a2 2 0 0 1-.874-.874C3 19.48 3 18.92 3 17.8V6.2Z" />
+					<path d="M14 6.2c0-1.12 0-1.68.218-2.108a2 2 0 0 1 .874-.874C15.52 3 16.08 3 17.2 3h.6c1.12 0 1.68 0 2.108.218a2 2 0 0 1 .874.874C21 4.52 21 5.08 21 6.2v11.6c0 1.12 0 1.68-.218 2.108a2 2 0 0 1-.874.874C19.48 21 18.92 21 17.8 21h-.6c-1.12 0-1.68 0-2.108-.218a2 2 0 0 1-.874-.874C14 19.48 14 18.92 14 17.8V6.2Z" />
 				</svg>
 			</IconButton>
 			{ready && !disabled ? <ManagerRenderedGridsContainer /> : null}
