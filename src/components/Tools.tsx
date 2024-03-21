4cbb1b3a01150428253ae5718c4ac4be69695f0b
Marais Rossouw <me@marais.co>
2024-03-21 12:15:18 +1000

    chore: the icon is a little too big now, lets make it smaller

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -93,8 +93,8 @@ export function Tools() {
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 24 24"
-					width="24"
-					height="24"
+					width="14"
+					height="14"
 					fill="none"
 					stroke="currentColor"
 					strokeLinecap="round"
