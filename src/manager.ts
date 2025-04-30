127f63242b0ec79529f3647794a920c715a342b3
Marais Rossouw <me@marais.co>
2025-04-30 10:27:03 +1000

    chore: move Tools -> Tool to better align with docs

--- src/manager.ts
+++ src/manager.ts
@@ -1,7 +1,7 @@
 import { addons, types } from '@storybook/manager-api';
 import { ADDON_ID, PARAM_KEY, PANEL_ID } from './constants';
 
-import { Tool } from './Tools';
+import { Tool } from './Tool';
 
 addons.register(ADDON_ID, () => {
 	addons.add(PANEL_ID, {
